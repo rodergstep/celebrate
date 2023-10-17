8c8043ef2b94eddb0000541ed5cf5fe5d624d6d5
rodergstep <rodergstep@gmail.com>
2023-10-17 14:12:16 +0300

    updated swagger options

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,9 +10,23 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Celebrate docs')
     .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  SwaggerModule.setup('swagger', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   await app.listen(3001);
 }
